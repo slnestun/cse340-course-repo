362d10f3f917607a4833e1e78828daa8995d5b32
slnestun <riv21007@byui.edu>
2026-09-04 10:35:14 -0300

    fix ssl conection

--- src/models/db.js
+++ src/models/db.js
@@ -13,7 +13,11 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: process.env.NODE_ENV === 'production'
+        ? false
+        : {
+            rejectUnauthorized: true
+        }
 });
 
 /**
